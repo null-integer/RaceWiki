b2ce6e57659cb4cc2b816736a6293129569b71f3
alfonso <a.barrera.29123@gmail.com>
2023-04-15 01:52:52 -0500

    added mock db

--- server.js
+++ server.js
@@ -22,6 +22,51 @@ categories = [
   "Nascar"
 ];
 
+let mockDatabase = {
+  "category": [
+    {"categoryID":0, "categoryName":"F1", "description":"", "rules":"", "Drivers": 20, "Teams": 10, "pictureURL":"" },
+    {"categoryID":1, "categoryName":"F2", "description":"", "rules":"","Drivers": 22, "Teams": 11, "pictureURL":""},
+    {"categoryID":2, "categoryName":"F3", "description":"", "rules":"","Drivers": 30, "Teams": 10, "pictureURL":""},
+    {"categoryID":3, "categoryName":"IndyCar","description":"", "rules":"","Drivers": 34, "Teams": 12, "pictureURL":""},
+    {"categoryID":4, "categoryName":"MotoGP", "description":"", "rules":"","Drivers": 30, "Teams": 15, "pictureURL":""},
+    {"categoryID":5, "categoryName":"Moto2", "description":"", "rules":"","Drivers": 30, "Teams": 15, "pictureURL":""},
+    {"categoryID":6, "categoryName":"Nascar", "description":"", "rules":"","Drivers": 30, "Teams": 15, "pictureURL":""},
+  ],
+  "flagType": [
+    {"flagTypeID":0, categoryID: 0, "flagMeaning": "", "pictureURL":""}
+  ],
+  "season":[
+    {"seasonID":0, "categoryID":0, "seasonYear":2020, "scoringSystem":" "}
+  ],
+  "raceWeekend":[
+    {"raceWeekendID":0, "categoryID":0, "raceDate":"", "practiceSessionID":0, "qualifyingID":0, "raceID":0, "circuitID":0, "RaceName":""}
+  ],
+  "PracticeSession":[
+    {"practiceSessionID":0, "sessionDate":"", "sessionTime":"", "resultID":0, "weather":" "}
+  ],
+  "QualifyingSession":[
+    {"qualifyingSessionID":0, "sessionDate":"", "sessionTime":"", "resultID":0, "weather":" "}
+  ],
+  "QualifyingSession":[
+    {"qualifyingSessionID":0, "sessionDate":"", "sessionTime":"", "resultID":0, "weather":" "}
+  ],
+  "Race":[
+    {"RaceID":0, "raceDate":"", "racetime":"", "resultID":0, "weather":"", "raceDuration":0, "laps":0, "fastestLaptime":"","fastestLapDriverID":0}
+  ],
+  "result":[
+    {"raceweekendID":0, "driverID":0, "sessionType":"", "position":0, "lapsLead":0}
+  ],
+  "team":[
+    {"teamID":0, "categoryID":0, "TeamName":" ", "baseLocation":" ", "pictureURL":" "}
+  ],
+  "driver":[
+    {"driverID":0, "firstName":" ", "lastName":" ", "driverNumber": "", "DoB":"", "nationality":" ", "penaltyPoints":" ", "pictureURL":""}
+  ],
+  "drove":[
+    {"seasonID":0, "teamID":0, "driverID":0}
+  ]
+};
+
 //Routes
 
 //Main Homepage
@@ -49,6 +94,7 @@ app.get('/category/:categoryName', async (req, res) =>{
 
   if (categories.includes(req.params['categoryName'])){
     let sections = [
+      "Description",
       "Rules",
       "Flags",
       "Championship",
